372a2938ae5886d040d4c30b62c673eaf4d3a70c
James Bennett <2009bennettj@gmail.com>
2023-01-14 20:43:15 +0100

    SQL JS DB

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,18 +14,18 @@ const RESOURCES = {
 "splash/img/light-1x.png": "f75f8e4ef725112d64537c9485bd3409",
 "splash/splash.js": "123c400b58bea74c1305ca3ac966748d",
 "splash/style.css": "c9d553ad6768c36e70713fdc6434f893",
-"index.html": "4688afca8df5dbfd96b1f4ef3142aa6d",
-"/": "4688afca8df5dbfd96b1f4ef3142aa6d",
-"main.dart.js": "3e52fa3358950e4f5b77636969a338a4",
-"flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
+"index.html": "95a22a16378301b4e4e580929c116622",
+"/": "95a22a16378301b4e4e580929c116622",
+"main.dart.js": "bd256aa55b94852aa272e9195c275570",
 "favicon.png": "7e4b8b2273112f4dd8cbf02036f36ac2",
+"sql-wasm.js": "7ce6f6c33529a9703e33e194e79b7691",
 "icons/Icon-192.png": "45c47fa673f70524b3ffaca243e89094",
 "icons/Icon-maskable-192.png": "45c47fa673f70524b3ffaca243e89094",
 "icons/Icon-maskable-512.png": "477d3d516651767eb83ae939a330628e",
 "icons/Icon-512.png": "477d3d516651767eb83ae939a330628e",
 "manifest.json": "af7ffeabdd3d1efa6d4a6bf3621fa83d",
 "assets/AssetManifest.json": "46f198f611d2a86ac22178553233b7d6",
-"assets/NOTICES": "5a3ff8715ecc04b33cea076c2dddf1f2",
+"assets/NOTICES": "7a7fada3bab8023836231b29ad614f6c",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
@@ -44,10 +44,7 @@ const RESOURCES = {
 "assets/assets/images/flower_shadow_ios.png": "b8cb680dd8b386d943be535279ebe6e3",
 "assets/assets/images/flower_web.png": "a05329734280f741ced1cf4edcd38ecd",
 "assets/assets/images/flower_bg.png": "f60fefe372c1fa283ce0661b7e5438af",
-"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
-"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
-"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
-"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e"
+"sql-wasm.wasm": "9acb7a2acdb22234cf19be0aa2f332d0"
 };
 
 // The application shell files that are downloaded before a service worker can
